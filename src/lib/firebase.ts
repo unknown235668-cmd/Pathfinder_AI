b2e960c6e84fe249286d13de17f18c9270c9d651
unknown <unknown235668@gmail.com>
2025-09-05 06:42:20 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  projectId: "pathfinder-ai-xsk6g",
+  appId: "1:686789703927:web:cd9cfb6ea066e1a52f7bf2",
+  storageBucket: "pathfinder-ai-xsk6g.firebasestorage.app",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "pathfinder-ai-xsk6g.firebaseapp.com",
+  messagingSenderId: "686789703927",
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
